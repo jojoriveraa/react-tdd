f005d8270b3d8aeab5534031ee70e29853889232
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-03 22:02:47 +0000

    Deploy jojoriveraa/giftgiver to github.com/jojoriveraa/giftgiver.git:gh-pages

---
+++ precache-manifest.012472f220140a702221e0bb8186b029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0f543350b61a793cb312cbf3913587",
+    "url": "/giftgiver/index.html"
+  },
+  {
+    "revision": "8e49761d9f35803af939",
+    "url": "/giftgiver/static/css/main.b33ee5f7.chunk.css"
+  },
+  {
+    "revision": "214d74b9d4318cecd3ea",
+    "url": "/giftgiver/static/js/2.7fffa16b.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/giftgiver/static/js/2.7fffa16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e49761d9f35803af939",
+    "url": "/giftgiver/static/js/main.52e4c155.chunk.js"
+  },
+  {
+    "revision": "d136d96d12051cfc0bc3",
+    "url": "/giftgiver/static/js/runtime-main.172548b3.js"
+  }
+]);
